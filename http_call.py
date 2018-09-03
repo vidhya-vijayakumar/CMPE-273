3e690324cdf353556eeed279f198a6ddf436a20e
vidhya-vijayakumar <36356627+vidhya-vijayakumar@users.noreply.github.com>
2018-09-02 21:39:45 -0700

    Add files via upload

---
+++ http_call.py
@@ -0,0 +1,12 @@
+import requests
+import random
+
+def a(pid):
+        r = requests.get('https://webhook.site/6a0cc4e9-3ffd-40e7-b972-ec191f421cf0')
+        print (r.headers['Date'])
+
+def main():
+    for i in range(1, 4):
+        a(i)
+
+main()
